51b828604e2ea32dd9119725aa09e9f35176c0f2
celestine <lestine603@gmail.com>
2022-08-10 23:56:19 +0100

    More functionality added to the add cart section with javascript

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const cartDetails = document.querySelector(".cart-details");
 const count = document.querySelector(".count")
 const minus = document.querySelector(".minus")
 const plus = document.querySelector(".plus")
+const addToCart = document.querySelector(".add-cart")
+const notify = document.querySelector(".notify")
 
 shoppingIcon.addEventListener("click", () => {
     if (cartDetails.classList.contains("cart-closed")) {
@@ -23,4 +25,13 @@ minus.addEventListener("click", () => {
         count.innerHTML--
     }
 })
+
+addToCart.addEventListener("click", () => { 
+    if(notify.classList.contains("show")) {
+        notify.classList.remove("show")
+    }
+    notify.innerHTML++
+    count.innerHTML = 0 
+})
+
 
